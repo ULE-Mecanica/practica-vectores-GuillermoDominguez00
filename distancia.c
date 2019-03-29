4b6956e482abfb0c9bdf8009ac3625b4ca2b438a
GuillermoDominguez00 <47889916+GuillermoDominguez00@users.noreply.github.com>
2019-03-29 13:36:47 +0100

    Ejercicio 1 Práctica Vectores

---
+++ distancia.c
@@ -0,0 +1,30 @@
+#include
+#include
+#define TAM 2
+void cargar(int V[2]);
+float distancia(int V1[2], int V2[2]);
+float distancia(V1*V2);
+int main()
+{
+	int V1[2];
+	int V2[2];
+	cargar (V1);
+	cargar (V2);
+	void cargar (int V[2]){
+		int i;
+		for(i=0;i scanf("%d",&V[i]);
+	}
+}
+float de = distancia(V1,V2);
+printf("%f \n",de);
+return 0;
+float distancia (int V1[2], int V2[2]);
+{
+	int resta1;
+	int resta2;
+	resta1=V2[0]-V1[0];
+	resta2=V2[1]-V2[0];
+	int raiz =sqrt(pow(resta1,2)+pow(resta2,2));
+}
+return main;
+
